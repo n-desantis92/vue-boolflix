ba7386fb08d86795bcac2374e4d88d94ca96bd5a
n-desantis92 <dsnwebdev@gmail.com>
2021-04-12 14:50:06 +0200

    aggiunta ricerca alle serie tv

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var app = new Vue ({
   el: "#app",
   data: {
-    titoloFilm: "futuro",
+    titoloFilm: "scrubs",
     listaFilms: [],
   },
   methods: {
@@ -26,10 +26,30 @@ var app = new Vue ({
 
       });
 
+      axios.get('https://api.themoviedb.org/3/search/tv?', {
+          params: {
+              api_key: "b04973b27729a64bccaff42ef0c92870",
+              query: this.titoloFilm,
+              language: "it-IT",
+          }
+      })
+      .then((response) => {
+          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
+          this.listaFilms = response.data.results;
+          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
+          console.log(this.listaFilms);
+          // trasformo il voto da 1 a 10 ad 1 a 5
+          this.listaFilms.forEach((item, i) => {
+            this.listaFilms[i].vote_average = Math.ceil(item.vote_average / 2);
+          });
+          // /trasformo il voto da 1 a 10 ad 1 a 5
+
+      });
+
       // porto il valore di ricerca a vuoto
       this.titoloFilm = "";
       // /porto il valore di ricerca a vuoto
-      
+
     },
 
 
