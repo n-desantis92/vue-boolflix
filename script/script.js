f0665d0d4a4eb7619b26697bdcf340d5b04dd875
n-desantis92 <dsnwebdev@gmail.com>
2021-04-13 16:03:13 +0200

    corretta chiamate axios

--- script/script.js
+++ script/script.js
@@ -1,54 +1,48 @@
 var app = new Vue ({
   el: "#app",
   data: {
-    titoloFilm: "scrubs",
+    url: "https://api.themoviedb.org/3/search/",
+    key: "b04973b27729a64bccaff42ef0c92870",
+    titoloFilm: "",
     listaFilms: [],
   },
   methods: {
-    search: function () {
-      axios.get('https://api.themoviedb.org/3/search/movie?', {
-          params: {
-              api_key: "b04973b27729a64bccaff42ef0c92870",
-              query: this.titoloFilm,
-              language: "it-IT",
-          }
-      })
-      .then((response) => {
-          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
-          this.listaFilms = response.data.results;
-          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
-          console.log(this.listaFilms);
-          // trasformo il voto da 1 a 10 ad 1 a 5
-          this.listaFilms.forEach((item, i) => {
-            this.listaFilms[i].vote_average = Math.ceil(item.vote_average / 2);
-          });
-          // /trasformo il voto da 1 a 10 ad 1 a 5
-
-      });
-
-      axios.get('https://api.themoviedb.org/3/search/tv?', {
-          params: {
-              api_key: "b04973b27729a64bccaff42ef0c92870",
-              query: this.titoloFilm,
-              language: "it-IT",
-          }
-      })
-      .then((response) => {
-          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
-          this.listaFilms = response.data.results;
-          // vado a prendere i dati generali che mi servono e li inserisco in un'oggetto
-          console.log(this.listaFilms);
-          // trasformo il voto da 1 a 10 ad 1 a 5
-          this.listaFilms.forEach((item, i) => {
-            this.listaFilms[i].vote_average = Math.ceil(item.vote_average / 2);
-          });
-          // /trasformo il voto da 1 a 10 ad 1 a 5
-
-      });
-
-      // porto il valore di ricerca a vuoto
+    search() {
+      let serchquery = this.titoloFilm;
       this.titoloFilm = "";
-      // /porto il valore di ricerca a vuoto
+
+      axios.get(this.url + 'movie', {
+          params: {
+            api_key: this.key,
+            query: serchquery,
+            language: "it-IT",
+          }
+      })
+      .then((response) => {
+
+        this.listaFilms = response.data.results;
+
+        axios.get(this.url + 'tv', {
+          params: {
+            api_key: this.key,
+            query: serchquery,
+            language: "it-IT",
+          }
+        })
+        .then((response) => {
+          this.listaFilms = [...this.listaFilms,...response.data.results];
+          console.log(this.listaFilms);
+
+          this.listaFilms.forEach((item, i) => {
+            item.vote_average = Math.ceil(item.vote_average / 2).toFixed();
+            console.log(item.vote_average);
+          });
+
+        });
+
+
+      });
+
 
     },
 
